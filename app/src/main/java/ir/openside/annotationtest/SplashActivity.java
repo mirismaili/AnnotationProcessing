21a1fd6e49f3b862f36b71feabd66e066bb04d47
S. Mahdi Mir-Ismaili <s.m.mirismaili@gmail.com>
2018-08-27 03:11:06 +0430

    Initial commit

---
+++ app/src/main/java/ir/openside/annotationtest/SplashActivity.java
@@ -0,0 +1,20 @@
+package ir.openside.annotationtest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.annotationsample.Navigator;
+
+/**
+ * Created by S. Mahdi Mir-Ismaili on 1397/6/4 (26/08/2018).
+ * <p><a href="mailto:s.m.mirismaili@gmail.com">s.m.mirismaili@gmail.com</a>
+ */
+public class SplashActivity extends AppCompatActivity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Navigator.startMainActivity(this); //generated class, method
+	}
+}
